cfcf6104b194ad016f8e982ee9a8ebec58350f15
thang <phanvanthang9118@gmail.com>
2018-08-12 09:15:58 +0700

    update

--- session 10/ex1.js
+++ session 10/ex1.js
@@ -5,33 +5,54 @@ function validateForm() {
 	var email = document.getElementById('email').value;
 	var noid = document.getElementById('noid').value;
 	var tde = document.getElementById('tde').value;
-	var male = document.getElementById('noid').checked;
-	var other = document.getElementById('noid').checked;
-	if(user =="" && date =="" && phone =="" && email == "" && noid =="" && male == false && other == false ){
-		if(user == ""){
-			document.getElementById('tuser').innerHTML = 'vui lòng nhập tên';
-		}
-		if(date == ""){
-			document.getElementById('tdate').innerHTML = 'vui lòng nhập ngày sinh';
-		}
-		if(phone == ""){
-			document.getElementById('tphone').innerHTML = 'vui lòng nhập số điện thoại';
-		}
-		if(email == ""){
-			document.getElementById('temail').innerHTML = 'vui lòng nhập email';
-		}
-		if(tde == ""){
-			document.getElementById('td').innerHTML = 'vui lòng nhập tiêu đề';
-		}
-		if(noid == ""){
+	var male = document.getElementById('male').checked;
+	var female = document.getElementById('female').checked;
+	var other = document.getElementById('other').checked;
+	var checked = true;
+	if(user == ""){
+		document.getElementById('tuser').innerHTML = 'vui lòng nhập tên';
+		checked = false;
+	}else{
+		document.getElementById('tuser').innerHTML = '';
+	}
+	if(date == ""){
+		document.getElementById('tdate').innerHTML = 'vui lòng nhập ngày sinh';
+		checked = false;
+	}else{
+		document.getElementById('tdate').innerHTML = '';
+	}
+	if(phone == ""){
+		document.getElementById('tphone').innerHTML = 'vui lòng nhập số điện thoại';
+		checked = false;
+	}else{
+		document.getElementById('tphone').innerHTML = '';
+	}
+	if(email == ""){
+		document.getElementById('temail').innerHTML = 'vui lòng nhập email';
+		checked = false;
+	}else{
+		document.getElementById('temail').innerHTML = '';
+	}
+	if(tde == ""){
+		document.getElementById('td').innerHTML = 'vui lòng nhập tiêu đề';
+		checked = false;
+	}else{
+		document.getElementById('td').innerHTML = '';
+	}
+	if(noid == ""){
 			document.getElementById('nd').innerHTML = 'vui lòng nhập tiêu đề';
-		}
-		if (male == false && other == false) {
-			document.getElementById('gender').innerHTML = 'vui lòng nhập giới tính';
-		}
+			checked = false;
+	}else{
+		document.getElementById('nd').innerHTML = '';
 	}
-	else{
-		document.write('bạn đã đăng ký thành công')
+	if (male == false && other == false && female == false) {
+		document.getElementById('gender').innerHTML = 'vui lòng nhập giới tính';
+		checked = false;
+	}else{
+		document.getElementById('gender').innerHTML = '';
 	}
-
+	if(checked){
+		document.getElementById('all').style.display = 'none';
+		document.getElementById('dky').style.display = 'block';
+	}
 }
